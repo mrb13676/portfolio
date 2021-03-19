21fc185189cab8fd3f075233b6b97ac53999e2bb
Mike Blackburn <jhb.anaesthetist@gmail.com>
2021-03-19 09:34:45 +0200

    friday 19 mar

---
+++ script.js
@@ -0,0 +1,13 @@
+function displayDemo() {
+
+}
+var elmnt = document.getElementsByTagName("footer");
+
+function scrollToTop() {
+  elmnt.scrollIntoView(true); // Top
+}
+
+var elmnt1 = document.getElementsById("about-me")
+function scrollToBottom() {
+  elmnt1.scrollIntoView(true); // Bottom
+}
